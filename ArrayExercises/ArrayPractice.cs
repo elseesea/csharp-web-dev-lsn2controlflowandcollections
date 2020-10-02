e9b5dc7a579f60aeef8902bfd9b7f6824a8e5361
lcc <sub01@iaonline.net>
2020-10-01 19:13:37 -0500

    Initial commit of Array Practice

---
+++ ArrayExercises/ArrayPractice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayExercises
+{
+    class ArrayPractice
+    {
+        public static int[] intArry = { 1, 1, 2, 3, 5, 8 };
+    }
+}
--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -6,7 +6,26 @@ namespace ArrayExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Arrays!");
+            Console.WriteLine("Using for loop");
+            for (int i=0; i<ArrayPractice.intArry.Length; i++)
+            {
+                Console.WriteLine(ArrayPractice.intArry[i]);
+            }
+
+            Console.WriteLine("Using foreach loop");
+            foreach (int anInt in ArrayPractice.intArry)
+            {
+                Console.WriteLine(anInt);
+            }
+
+            Console.WriteLine("Using foreach loop, odd numbers only");
+            foreach (int anInt in ArrayPractice.intArry)
+            {
+                if (anInt%2 != 0)
+                {
+                Console.WriteLine(anInt);
+                }
+            }
         }
     }
 }
