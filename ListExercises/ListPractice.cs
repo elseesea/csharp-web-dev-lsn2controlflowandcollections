b36d7c6b666757959b411164077d47372d0aff1e
lcc <sub01@iaonline.net>
2020-10-01 19:56:36 -0500

    Second commit of ListPractice - added method for searching string of user-given length

---
+++ ListExercises/ListPractice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListExercises
+{
+    class ListPractice
+    {
+        public static int GetSumEven(List<int> lstInts)
+        {
+            int sumOfEvens = 0;
+
+            foreach (int anInt in lstInts)
+            {
+                if (anInt%2 == 0)
+                {
+                    sumOfEvens += anInt;
+                }
+            }
+            return sumOfEvens;
+        }
+
+        public static void Print5LetterWords(List<string> lstWords, int searchLength = 5)
+        {
+            foreach (string aWord in lstWords)
+            {
+                if (aWord.Length == searchLength)
+                {
+                    Console.WriteLine(aWord);
+                }
+            }
+        }
+    }
+}
--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ListExercises
 {
@@ -6,7 +7,14 @@ namespace ListExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Lists!");
+            //Console.WriteLine("Hello Lists!");
+            List<int> lstNumbers = new List<int> { 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610};
+            Console.WriteLine(ListPractice.GetSumEven(lstNumbers));
+
+            List<string> lstString = new List<string> { "hello", "good", "thief", "steal", "bye", "time", "remote", "power" };
+            Console.WriteLine("Search for words of what length?");
+            string userInput = Console.ReadLine();
+            ListPractice.Print5LetterWords(lstString, int.Parse(userInput));
         }
     }
 }
